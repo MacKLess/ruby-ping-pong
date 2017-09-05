c2d28fdd75ea472cc933cdb56eb175bbafb962e1
kelsey langlois and LDMacKrell <kelsey@>
2017-09-05 09:23:10 -0700

    Update ping_pong to return list of numbers in range input

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,5 +1,11 @@
 class Integer
   def ping_pong()
-
+    number = 1
+    results = []
+    times() do
+      results.push(number)
+      number = number + 1
+    end
+    results
   end
-end    
+end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -2,5 +2,7 @@ require('rspec')
 require('ping_pong')
 
 describe('#ping_pong') do
-  # 
+  it("counts up from 1 to the number entered") do
+    expect((4).ping_pong()).to(eq([1, 2, 3, 4]))
+  end
 end
