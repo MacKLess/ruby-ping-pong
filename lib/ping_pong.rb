38ad345749c7e91d63e8a38b639ed32aa4d2abc7
kelsey langlois and LDMacKrell <ldmackrell@>
2017-09-05 09:38:24 -0700

    Update ping_pong to replace numbers divisible by 15 with ping-pong.

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,10 +3,12 @@ class Integer
     number = 1
     results = []
     times() do
-      if number % 3 == 0
+      if number % 15 == 0
+        results.push('ping-pong')
+      elsif number % 3 == 0
         results.push('ping')
       elsif number % 5 == 0
-        results.push('pong')  
+        results.push('pong')
       else
         results.push(number)
       end
@@ -15,3 +17,4 @@ class Integer
     results
   end
 end
+puts 15.ping_pong()
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -13,4 +13,8 @@ describe('#ping_pong') do
   it("replaces numbers divisible by 5 with 'pong'") do
     expect((5).ping_pong()).to(eq([1, 2, 'ping', 4, 'pong']))
   end
+
+  it("replaces numbers divisible by 15 with 'ping-pong'") do
+    expect((15).ping_pong()).to(eq([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'ping-pong']))
+  end
 end
